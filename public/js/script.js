519f9a926b3e84e5ebc4cf7b9d86699c6c160fbf
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-07-05 11:39:21 +0100

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,18 @@
+window.onload = function() {
+    document.getElementById('titre').addEventListener('click', function() {
+        location.reload(); // Refresh the page
+    });
 
-document.getElementById('titre').addEventListener('click', function() {
-    location.reload(); // Refresh the page
-  });
+    function redirigerVersWhatsApp() {
+        window.location.href = "https://wa.me/+237620114013";
+    }
 
+    document.getElementById('profile-pic').addEventListener('click', function() {
+        location.reload(); // Refresh the page
+    });
 
-
-
-  function redirigerVersWhatsApp() {
-    window.location.href = "https://wa.me/+237620114013";
-  }
-
-
-
-  document.getElementById('profile-pic').addEventListener('click', function() {
-    location.reload(); // Refresh the page
-  });
-
-
-
-  function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('show');
-  }
+    function toggleSidebar() {
+        const sidebar = document.getElementById('sidebar');
+        sidebar.classList.toggle('show');
+    }
+};
