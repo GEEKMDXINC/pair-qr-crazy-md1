a129dce82360adb0456bd4ff7dacd5216cd3845c
GEEKMDXINC <herukobot67@gmail.com>
2025-02-02 03:37:00 +0100

    Update id.js

--- id.js
+++ id.js
@@ -1,10 +1,32 @@
-function makeid(num = 4) {
+
+
+function makeid() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  for (let i = 0; i < 10; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+  return result ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ /* function makeid(num = 4) {
   let result = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var characters9 = characters.length;
   for (var i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
   }
-  return result;
+  return result; */
 }
 module.exports = {makeid};
